2a98f97a5240f92656c8d9682829b32f08d3f9f7
samanvaya <samanvayavats@gmail.com>
2025-08-19 14:50:07 +0530

    setup the nextauth in the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+'use client'
+
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (!session) {
+    return (
+      <div className="p-6 min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold mb-4">Login Required</h1>
+        <button
+          onClick={() => signIn('google')}
+          className="bg-black text-white px-4 py-2 rounded"
+        >
+          Login with Google
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div>
-       this is the div 
-    </div> 
+    <div
+    >
+    {session.user?.email}
+      
+    </div>
   );
 }
