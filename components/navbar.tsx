f604cf7eb55e44ae2ec647061d9358cdfcd64419
samanvaya <samayvats@gmail.com>
2025-08-25 00:07:38 +0530

    made the frontend logic for the navbar to handle the cases is like if not login , login , login but not registered and last one login and registered

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TiThMenu } from "react-icons/ti";
 import { GrClose } from "react-icons/gr";
 import {
@@ -8,11 +8,32 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import Link from "next/link";
-
+import axios from 'axios'
 import { useSession, signIn, signOut } from 'next-auth/react';
+
 const Navbar = () => {
+  const [isRegistered, setIsRegistered] = useState(false);
   const [open, setOpen] = useState(false);
-  const { data: session } = useSession()
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const checkProfile = async () => {
+      if (session?.user?.email) {
+        try {
+
+          const { data } = await axios.get(`/api/user/isprofilecreated?email=${session.user.email}`);
+          setIsRegistered(data?.isRegistered || false);
+          console.log('data:', data);
+        } catch (error: any) {
+          // console.error(error);
+          console.error(error?.response?.data?.message, error?.status);
+        }
+      }
+    };
+    checkProfile();
+  }, [session?.user?.email]);
+
+  // the user has not login yet 
   if (!session) {
     return (<div>
       <Menubar className="bg-black flex justify-between w-auto px-4 rounded-2xl mb-2">
@@ -22,17 +43,11 @@ const Navbar = () => {
             VendorsBridge
           </MenubarTrigger>
         </MenubarMenu>
-
         {/* Desktop Menu */}
         <div className="hidden md:flex space-x-6 pr-10 text-white">
           <MenubarMenu>
             <Link href='/'><MenubarTrigger>Home</MenubarTrigger></Link>
-          </MenubarMenu>
-          <MenubarMenu>
             <MenubarTrigger>About</MenubarTrigger>
-          </MenubarMenu>
-
-          <MenubarMenu >
             <MenubarTrigger onClick={() => signIn('google')}>Login</MenubarTrigger>
           </MenubarMenu>
 
@@ -57,9 +72,53 @@ const Navbar = () => {
     </div>)
   }
 
+  // the user has login and created the store as well
+  if (session && isRegistered) {
+    return (
+      <div>
+        <Menubar className="bg-black flex justify-between w-auto px-4 rounded-2xl mb-2">
+          {/* Logo */}
+          <MenubarMenu>
+            <MenubarTrigger className="text-white font-bold text-lg">
+              VendorsBridge
+            </MenubarTrigger>
+          </MenubarMenu>
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-6 pr-10 text-white">
+            <MenubarMenu>
+              <Link href='/'><MenubarTrigger>Home</MenubarTrigger></Link>
+              <MenubarTrigger>About</MenubarTrigger>
+              <Link href='/onlinestore/ok'><MenubarTrigger>MyStore</MenubarTrigger></Link>
+              <Link href='/dashboard/ok'><MenubarTrigger>Dashboard</MenubarTrigger></Link>
+              <MenubarTrigger onClick={() => signOut()}>Logout</MenubarTrigger>
+            </MenubarMenu>
+          </div>
+
+          {/* Mobile Hamburger */}
+          <div className="md:hidden text-white">
+            <button onClick={() => setOpen(!open)}>
+              {open ? <GrClose size={28} /> : <TiThMenu size={28} />}
+            </button>
+          </div>
+        </Menubar>
+
+        {/* Mobile Dropdown */}
+        {open && (
+          <div className="md:hidden bg-black text-white flex flex-col space-y-2 py-3 items-center mx-3 mb-3 rounded-2xl">
+            <Link href='/'>
+              <button className="text-left">Home</button>
+            </Link>
+            <button className="text-left">About</button>
+            <Link href='/onlinestore/ok'><button className="text-left">MyStore</button></Link>
+            <Link href='/dashboard/ok'><button className="text-left">Dashboard</button></Link>
+            <button className="text-left" onClick={() => signOut()}>Logout</button>
+          </div>
+        )}
+      </div>
+    )}
 
 
-
+  // user has just login but did not have created the store yet 
   return (
     <div>
       <Menubar className="bg-black flex justify-between w-auto px-4 rounded-2xl mb-2">
@@ -74,14 +133,10 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-6 pr-10 text-white">
           <MenubarMenu>
             <Link href='/'><MenubarTrigger>Home</MenubarTrigger></Link>
-          </MenubarMenu>
-          <MenubarMenu>
             <MenubarTrigger>About</MenubarTrigger>
-          </MenubarMenu>
-          <MenubarMenu>
-            <MenubarTrigger>CreateStore</MenubarTrigger>
-          </MenubarMenu>
-          <MenubarMenu>
+          <Link href='/storeregisterform'>
+              <MenubarTrigger>CreateStore</MenubarTrigger>
+          </Link>
             <MenubarTrigger onClick={() => signOut()}>Logout</MenubarTrigger>
           </MenubarMenu>
         </div>
@@ -97,14 +152,21 @@ const Navbar = () => {
       {/* Mobile Dropdown */}
       {open && (
         <div className="md:hidden bg-black text-white flex flex-col space-y-2 py-3 items-center mx-3 mb-3 rounded-2xl">
-          <button className="text-left">Home</button>
+          <Link href='/'>
+            <button className="text-left">Home</button>
+          </Link>
           <button className="text-left">About</button>
-          <button className="text-left">CreateStore</button>
+          <Link
+            href='/storeregisterform'
+          >
+            <button className="text-left">CreateStore</button>
+          </Link>
           <button className="text-left" onClick={() => signOut()}>Logout</button>
         </div>
       )}
     </div>
   );
+
 };
 
 export default Navbar;
